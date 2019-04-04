5b9ed3e858e4a021ab2f56090998873feaab96ee
jwmx9 <31551248+jwmx9@users.noreply.github.com>
2019-04-03 22:18:01 -0500

    Add files via upload

---
+++ CrimeAnalyzer/CrimeAnalyzer/CrimeStats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CrimeAnalyzer
+{
+    public class CrimeStats
+    {
+        public int Year;
+        public int Population;
+        public int ViolentCrime;
+        public int Murder;
+        public int Rape;
+        public int Robbery;
+        public int AggAssault;
+        public int PropertyCrime;
+        public int Burglary;
+        public int Theft;
+        public int MVT;
+
+
+        public CrimeStats(int year, int population, int violentCrime, int murder,
+                         int rape, int robbery, int aggravatedAssault, int propertyCrime,
+                         int burglary, int theft, int motorVehicleTheft)
+        {
+            Year = year;
+            Population = population;
+            ViolentCrime = violentCrime;
+            Murder = murder;
+            Rape = rape;
+            Robbery = robbery;
+            AggAssault = aggravatedAssault;
+            PropertyCrime = propertyCrime;
+            Burglary = burglary;
+            Theft = theft;
+            MVT = motorVehicleTheft;
+        }
+    }
+}
+
